08a91ad84d0be7a371a631e0def9cdc9b2870b42
廣木 <hiroki19960829@gmail.com>
2025-04-09 20:47:15 +0900

    Add files via upload

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,21 +1,8 @@
 // 変数numを定義します
 let num = 15; // 例えば、15
 
-// 変数numが3の倍数かどうかを調べます
-if (num % 3 === 0) {
-    console.log("3の倍数です");
-}
-else {
-    console.log("3の倍数ではありません");
-}
 
-// 変数numが5の倍数かどうかを調べます
-if (num % 5 === 0) {
-    console.log("5の倍数です");
-}
-else {
-    console.log("5の倍数ではありません");
-}
+
 
 
 
