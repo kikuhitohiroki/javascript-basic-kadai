fc541420e74f9e8636d1e864471e181ca8110842
廣木 <hiroki19960829@gmail.com>
2025-04-13 13:22:16 +0900

    Add files via upload

---
+++ kadai_020/event.js
@@ -0,0 +1,12 @@
+addBtn.addEventListener('click', () => {
+    
+    // li要素を新しく作成する
+    const childList = document.createElement('text');
+    childList.textContent = 'ボタンをクリックしました';
+  
+    // parentListの最初の子要素を削除する
+    parentList.removeChild(parentList.firstChild);
+  
+    // childListを追加する
+    parentList.appendChild(childList);
+  });
